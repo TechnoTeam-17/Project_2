4c5685de7cb263231d665847267355e7bbd57156
NergisRecel <nergisrecel@hotmail.com>
2023-08-29 21:10:33 +0200

    Siparis Gecmisini Bilgisayara Indirme (branch duzeltmesi)

---
+++ src/downloadOrderHistory.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import util.BaseDriver;
+import util.MyFunc;
+
+public class downloadOrderHistory extends BaseDriver {
+
+    @Test
+    public void orderhistorytest(){
+        driver.get("https://demowebshop.tricentis.com/");
+//login
+        WebElement login= driver.findElement(By.xpath("//*[@href='/login']"));
+        login.click();
+        WebElement email= driver.findElement(By.id("Email"));
+        email.sendKeys("ali06@gmail.com");
+        WebElement pass= driver.findElement(By.id("Password"));
+        pass.sendKeys("Benim.45");
+
+        WebElement loginButton=driver.findElement(By.cssSelector("input.login-button"));
+        loginButton.click();
+//profil sayfasi ve ardindan siparislere gecis
+        WebElement profil=driver.findElement(By.xpath("//*[@href='/customer/info']"));
+        profil.click();
+        MyFunc.Bekle(1);
+        WebElement orders=driver.findElement(By.xpath("//*[@href='/customer/orders']"));
+        orders.click();
+        MyFunc.Bekle(1);
+        JavascriptExecutor js=(JavascriptExecutor) driver;//casting
+        js.executeScript("window.scrollTo(0, 500)"  );
+//siparis detayina giris ve fatura yukleme
+        MyFunc.Bekle(1);
+        WebElement firstOrderDetail=driver.findElement(By.cssSelector("input[value='Details']"));
+        firstOrderDetail.click();
+        MyFunc.Bekle(1);
+
+        WebElement download=driver.findElement(By.xpath("//*[@href='/orderdetails/pdf/1501518']"));
+        download.click();
+
+        BekleVeKapat();
+
+    }}
