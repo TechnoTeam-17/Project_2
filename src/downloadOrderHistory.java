f491265b3103f8ffa2a5c7d9a052e12a086ef14a
NergisRecel <141259522+NergisRecel@users.noreply.github.com>
2023-08-29 23:12:48 +0200

    Siparis (#5)
    * Siparis Gecmisini Bilgisayara Indirme (branch duzeltmesi)
    
    * Siparis Gecmisini Bilgisayara Indirme (yukleme adimindaki locatorin duzeltilmesi)
    
    * Siparis Gecmisini Bilgisayara Indirme (yukleme adimindaki locatorin duzeltilmesi)
    
    ---------
    
    Co-authored-by: TechnoTeam-17 <142696425+TechnoTeam-17@users.noreply.github.com>

--- src/downloadOrderHistory.java
+++ src/downloadOrderHistory.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.WebElement;
 import util.BaseDriver;
 import util.MyFunc;
 
+
+public class downloadOrderHistory extends BaseDriver {
+
 import java.util.List;
 import java.util.Set;
 
-public class downloadOrderHistory extends BaseDriver {
+
     @Test
     public void orderhistorytest(){
         driver.get("https://demowebshop.tricentis.com/");
@@ -29,21 +32,21 @@ public class downloadOrderHistory extends BaseDriver {
         WebElement orders=driver.findElement(By.xpath("//*[@href='/customer/orders']"));
         orders.click();
         MyFunc.Bekle(1);
-        JavascriptExecutor js=(JavascriptExecutor) driver;//casting
+
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+
         js.executeScript("window.scrollTo(0, 500)"  );
 //siparis detayina giris ve fatura yukleme
         MyFunc.Bekle(1);
         WebElement firstOrderDetail=driver.findElement(By.cssSelector("input[value='Details']"));
         firstOrderDetail.click();
-        MyFunc.Bekle(1);
 
-        WebElement download=driver.findElement(By.xpath("//*[@href='/orderdetails/pdf/1501518']"));
+        MyFunc.Bekle(2);
+
+        WebElement download=driver.findElement(By.linkText("PDF Invoice"));
+
         download.click();
 
         BekleVeKapat();
 
-
-
-
-    }
-}
+}}
